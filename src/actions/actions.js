b29326dd110dde3f6e60532b133e93dd0256e352
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2022-11-21 04:38:46 +0530

    readme updated

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 import {INITIAL_UPDATE_ALBUM_STORE,USER_EDITED_ALBUM,USER_CREATE_NEW_ALBUM} from './actionTypes'
 
 
-//exporting action type and data
+//exporting action type and data initially fetching data from server
 export function initialUpdateAlbumStore(albumData){
     return{
         type:INITIAL_UPDATE_ALBUM_STORE,
@@ -9,7 +9,7 @@ export function initialUpdateAlbumStore(albumData){
     }
 }
 
-//exporting action type and data
+//exporting action type and data when editing album is initiated
 export function userEditedAlbum(data){
     console.log('dispacthing')
     return{
@@ -18,7 +18,7 @@ export function userEditedAlbum(data){
     }
 }
 
-//exporting action type and data
+//exporting action type and data when create new entry initiated
 export function userCreatedNewdAlbum(data){
     return{
         type:USER_CREATE_NEW_ALBUM,
